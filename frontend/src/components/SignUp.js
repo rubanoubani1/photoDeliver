025a9fd6d75c179856b65ad3a97e7e05a2180471
Roope Herpiö <roope.herpio@gmail.com>
2022-05-05 14:55:38 +0300

    Added static version of sign up view

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,5 +1,13 @@
-const SignUp = () => {
+import SignUpForm from "./SignUpForm";
 
+const SignUp = () => {
+    return (
+        <div style={{padding:"5%", margin:"10%", backgroundColor:"aliceblue", borderRadius:"5%"}}>
+            <h2 style={{color:"blue"}}>Sign Up</h2>
+            <hr/>
+            <SignUpForm />
+        </div>
+    );
 }
 
 export default SignUp;
