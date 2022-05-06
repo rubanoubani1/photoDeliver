e3acc159e687e04d70dc6b5d02d65943ff822539
Petri Kärki <pjkarabc@saunalahti.fi>
2022-05-06 09:22:31 +0300

    Changed button text from Add picture to Add new photo

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () =>{
 
     return(
         <footer style={{backgroundColor:"lightblue",display:"flex",justifyContent:"center"}}>
-            <button style={{margin:"10px"}}>Add picture</button>
+            <button style={{margin:"10px"}}>Add new photo</button>
         </footer>
     )
 }
