423ec0cd6fc64997c29171c79e23c7ad057084b7
Petri Kärki <pjkarabc@saunalahti.fi>
2022-05-06 13:18:41 +0300

    Added logout page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {Routes,Route,Link} from 'react-router-dom';
+import {Routes,Route} from 'react-router-dom';
 import Home from './components/Home';
 import SavePage from './components/SavePage';
 import Settings from './components/Settings';
 import MyPosts from './components/MyPosts';
+import Logout from './components/Logout';
 
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/saved" element={<SavePage/>} />
         <Route path="/settings" element={<Settings/>} />
         <Route path="/posts" element={<MyPosts/>} />
+        <Route path="/Logout" element={<Logout/>} />
       </Routes>
     </div>
   );
