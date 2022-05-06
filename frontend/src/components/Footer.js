d9847312dd826a88b35d6f64c2bbaf8fe6a9216c
Roope Herpiö <roope.herpio@gmail.com>
2022-05-06 11:36:08 +0300

    made footer stay at the bottom of the screen

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,17 @@
-const Footer = () =>{
+const Footer = () => {
 
-    return(
-        <footer style={{backgroundColor:"lightblue",display:"flex",justifyContent:"center"}}>
-            <button style={{margin:"10px"}}>Add new photo</button>
+    return (
+        <footer style={{
+            backgroundColor: "lightblue",
+            display: "flex",
+            justifyContent: "center",
+            position: "fixed",
+            bottom: 0,
+            width: "100%"
+        }}>
+            <button style={{ margin: "10px" }}>Add new photo</button>
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
