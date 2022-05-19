b5c5753b823355b64821d0adb59490697e07ed58
rubanoubani1@gmail.com <rubanoubani1@gmail.com>
2022-05-19 12:07:09 +0300

    add login

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,88 +1,45 @@
-import LogInForm from './LogInForm';
+//import LogInForm from './LogInForm';
 import {useState, useEffect} from 'react';
-import Home from "./Home";
-import {Routes,Route,Link} from 'react-router-dom';
 
-
-const LogIn =() => {
+const LogIn =(props) => {
 	const[state,setState] = useState({
-		user:[],
-		msg:''
-	})
-	
-	const[urlRequest, setUrlRequest] = useState({
-			url:"",
-			request:{},
-			action:''
+		username:'',
+		password:''
 	})
 
-	useEffect(()=>{
-		
-		const fetchData = async() =>{
-			if(!urlRequest.url){
-				return;
+	const onChange = (event)=>{
+		setState((state)=>{
+			return {
+				...state,
+				[event.target.name]:event.target.value
 			}
-	
-			
-			if(urlRequest.action == "checkUser")	{
-				
-				let response = await fetch(urlRequest.url,urlRequest.request);
-				
-				if(response.ok) {
-					let data= await response.json();  
-					
 
-					if(data) {
-							
-						setState({
-							msg:''
-						})
-						
-						return(
-								<Routes>
-								
-						<Route  path="/" element={<Home/>} />
-						 </Routes>)
-						 
-						
-					}else{
-						
-						setState({
-							msg:'email or password not correct'						})
-						
-					}
-
-				}else {
-					
-					return
-				}
-			}else {
-				
-				return
-			}
-			
-		}
-
-		fetchData();
-	},[urlRequest.url,urlRequest.request]);
-
-	const getLogin = (data) =>{
-		setUrlRequest({
-			url:"/api/login",
-			request:{
-				method:"POST",
-				mode:"cors",
-				headers:{"Content-type":"application/json"},
-				body:JSON.stringify(data)
-			},
-			action:"checkUser"
 		})
 	}
 
+
+	let funcs = props.funcs;
+
+	const onSubmit =  (event) => {
+		event.preventDefault();
+		if(state.username.length < 4 || state.password.length < 8) {
+			funcs.setError("Username needs to be atleast four and password eight characters long");
+			return;
+		}
+		let user = {
+			username:state.username,
+			password:state.password
+		}
+		funcs.login(user);
+
+	}
+
+	
+
+    
 	return (
 		<div className="container py-5 h-100" style={{color:"E5E5E5"}}>
 			<div className="row d-flex justify-content-center align-items-center h-100">
-
 		
 				<div className="col-12 col-md-8 col-lg-6 col-xl-5" >
 					
@@ -94,7 +51,26 @@ const LogIn =() => {
 								<p>	{state.msg}</p>
 								
 								<br />
-							<LogInForm getLogin={getLogin}/>
+							<form onSubmit={onSubmit}>
+			<label htmlFor = "username">Email</label>
+			<input type="text"
+					name="username"
+					id="username"
+					onChange={onChange}
+					value={state.username} />
+			<br />
+			<br />
+			<label htmlFor = "password">Password</label>
+			<input type="text"
+					name="password"
+					id="password"
+					onChange={onChange}
+					value={state.password} />
+			<br />
+			<br />
+			<input type="submit" value="Log in"/>
+		</form>
+
 
 							<a href="url">forget password</a>
 							<br/>
