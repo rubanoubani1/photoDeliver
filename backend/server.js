324a604d15e4073d46c944ecbd518d9596100b9b
Roope Herpiö <roope.herpio@gmail.com>
2022-05-20 10:50:46 +0300

    Switch to mongodb database for users and sessions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const apiroutes = require("./routes/apiroutes");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const mongoose = require('mongoose');
+const userModel = require('./models/loginuser');
+const sessionModel = require('./models/session');
 
 const app = express();
 
@@ -14,6 +16,7 @@ const mongo_user = process.env.MONGODB_USER;
 const mongo_pw = process.env.MONGODB_PASSWORD;
 const mongo_url = process.env.MONGODB_URL;
 const mongo_db = "photoDeliverDatabase"
+const time_to_live_diff = 3600000;
 
 //MONGOOSE CONNECTION
 const connection_url = "mongodb+srv://" + mongo_user + ":" + mongo_pw + "@" + mongo_url + "/" + mongo_db + "?retryWrites=true&w=majority";
@@ -25,148 +28,6 @@ mongoose.connect(connection_url).then(
 
 mongoose.set("toJSON", { virtuals: true });
 
-//DATABASE
-
-const database = [
-	{
-		owner: {
-			firstname: "John",
-			lastname: "Doe",
-			id: 204,
-			urlsafe: "johndoe",
-			profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-		},
-		url: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg",
-		id: 102,
-		alt: "dog",
-		title: "Random dog picture",
-		date: "13 April 2022",
-		comments: [
-			{
-				user: {
-					firstname: "Jane",
-					lastname: "Doe",
-					id: 205,
-					urlsafe: "janedoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 123,
-				text: "such a cute dog!",
-				date: "13 April 2022"
-			}
-		],
-		bookmarked: "true"
-	},
-	{
-		owner: {
-			firstname: "John",
-			lastname: "Doe",
-			id: 204,
-			urlsafe: "johndoe",
-			profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-		},
-		url: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg",
-		id: 103,
-		alt: "dog",
-		title: "Random dog picture 2",
-		date: "14 April 2022",
-		comments: [],
-		bookmarked: "false"
-	},
-	{
-		owner: {
-			firstname: "John",
-			lastname: "Doe",
-			id: 204,
-			urlsafe: "johndoe",
-			profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-		},
-		url: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg",
-		id: 104,
-		alt: "dog",
-		title: "Random dog picture 3",
-		date: "15 April 2022",
-		comments: [
-			{
-				user: {
-					firstname: "Jane",
-					lastname: "Doe",
-					id: 205,
-					urlsafe: "janedoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 124,
-				text: "that's the third time you've posted that picture",
-				date: "15 April 2022"
-			},
-			{
-				user: {
-					firstname: "John",
-					lastname: "Doe",
-					id: 204,
-					urlsafe: "johndoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 125,
-				text: "It's the only one I have...",
-				date: "15 April 2022"
-			},
-			{
-				user: {
-					firstname: "Jane",
-					lastname: "Doe",
-					id: 205,
-					urlsafe: "janedoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 126,
-				text: "can't you take new photos?",
-				date: "15 April 2022"
-			},
-			{
-				user: {
-					firstname: "John",
-					lastname: "Doe",
-					id: 204,
-					urlsafe: "johndoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 127,
-				text: "My house burned and everything I own was destroyed so the only picture I have left is this one I had as my profile picture. Oh, poor Doge the third... If only I hadn't left the candles burning again while going to work.",
-				date: "15 April 2022"
-			},
-			{
-				user: {
-					firstname: "Jane",
-					lastname: "Doe",
-					id: 205,
-					urlsafe: "janedoe",
-					profilePictureUrl: "https://images.dog.ceo/breeds/terrier-border/n02093754_4072.jpg"
-				},
-				id: 128,
-				text: "wow that got dark fast",
-				date: "15 April 2022"
-			}
-		],
-		bookmarked: "false"
-	}
-];
-
-//LOGIN DATABASE
-let registeredUsers = [];
-let loggedSessions = [];
-let time_to_life_diff = 3600000;
-
-
-let testuser = {
-	email:'jane.doe@gmail.com',
-	password:'$2b$14$VuwxX7g049MbNc64V7ue2.q5TNiqtgnJFqtpxRDlGLG3f0E2I7Ol2'
-}
-
-registeredUsers.push(testuser);
-
-
-
 const createToken=() =>{
 	let token = crypto.randomBytes(128);
 	return token.toString("hex");
@@ -177,20 +38,36 @@ isUserLogged = (req,res,next)=>{
 	if(!req.headers.token) {
 		return res.status(403).json({message:"Forbidden!"});
 	}
-	for(let i=0; i<loggedSessions.length;i++){
-		if(req.headers.token===loggedSessions[i].token){
-			let now= Date.now();
-			if(now>loggedSessions[i].ttl){
-				loggedSessions.splice(i,1);
-				return res.status(403).json({message:"Forbidden!"});
-			}
-			loggedSessions[i].ttl=now+time_to_life_diff; // plus another hour
-			req.session = {};
-			req.session.user = loggedSessions[i].user;
-			return next();
+	
+	sessionModel.findOne({ "token": req.headers.token }, function (err, session) {
+		if (err) {
+			console.log("failed to find session while in filter, err: " + err);
+			return res.status(403).json({ message: "Forbidden" });
 		}
-	}
-	return res.status(403).json({message:"Forbidden!"});
+		if (!session) {
+			return res.status(403).json({ message: "Forbidden" });
+		}
+		let now = Date.now();
+		if (now > session.ttl) {
+			sessionModel.deleteOne({ "_id": session._id }, function (err) {
+				if (err) {
+					console.log("failed to remove expired session, err: " + err);
+				}
+				return res.status(403).json({ message: "Forbidden" });
+			});
+		} else {
+			req.session = {};
+			req.session.user = session.user;
+			session.ttl = now + time_to_live_diff;
+			session.save(function (err) {
+				if (err) {
+					console.log("failed to update session, err: " + err)
+				}
+				return next();//res.status(403).json({ message: "forbidden" });
+			})
+
+		}
+	})
 }
 
 
@@ -208,25 +85,26 @@ app.post("/register", function(req,res) {
 	if(req.body.username.length<4|| req.body.password.length<8) {
 		return res.status(400).json({message:"Please provide proper credentails"});
 	}
-	for(let i =0;i<registeredUsers.length;i++){
-		if(req.body.username===registeredUsers[i].username){
-			return res.status(409).json({message:"User already in use"});
+	
+	bcrypt.hash(req.body.password, 14, function (err, hash) {
+		if (err) {
+			return res.status(500).json({ message: "internal server error" });
 		}
-	}
-
-	bcrypt.hash(req.body.password,14 , function (err, hash){
-		if(err){
-			return res.status(500).json({message:"Internal server error"});
-		}
-		let user = {
-			username:req.body.username,
-			password:hash
-		}
-			
-		registeredUsers.push(user);
-		
-		return res.status(201).json({message:"Register Success"});
-	});
+		let user = new userModel({
+			username: req.body.username,
+			password: hash
+		});
+		user.save(function (err, user) {
+			if (err) {
+				console.log("Failed to save new user, error: " + err);
+				if (err.code === 11000) {
+					return res.status(409).json({ message: "username alreasy in use" });
+				}
+				return res.status(500).json({ message: "internal server error" });
+			}
+			return res.status(201).json({ message: "user registered" });
+		})
+	})
 
 	
 })
@@ -244,48 +122,52 @@ app.post("/login", function(req,res) {
 	if(req.body.username.length<4|| req.body.password.length<8) {
 		return res.status(400).json({message:"Please provide proper credentails"});
 	}
-	for(let i = 0;i<registeredUsers.length;i++){
-		
-		if(req.body.username===registeredUsers[i].email){
-		
-			bcrypt.compare(req.body.password, registeredUsers[i].password,function(err, success){
-				
-			if(err){
-					return res.status(500).json({message:"Internal server error"});
-				}
-				if(!success){
-					return res.status(401).json({message:"Unauthrized!"});
-				}
-				
-				let token = createToken();
-			
-				let now = Date.now();
-				let session = {
-					user:req.body.username,
-					token:token,
-					ttl:now+time_to_life_diff
-				}
-				loggedSessions.push(session);
-				return res.status(200).json({"token":token});
-			})
-
-			return;
+	userModel.findOne({ 'username': req.body.username }, function (err, user) {
+		if (err) {
+			console.log("error finding user, reason: " + err);
+			return res.status(500).json({ message: "Internal server error" });
 		}
-	}
-	return res.status(401).json({message:"Unauthrized!"});
+		if (!user) {
+			//TODO: wait 2 seconds for safety
+			return res.status(401).json({ message: "unauthorized" });
+		}
+		bcrypt.compare(req.body.password, user.password, function (err, success) {
+			if (err) {
+				console.log("Error comparing passwrds, err: " + err);
+				return res.status(500).json({ message: "Internal server error" });
+			}
+			if (!success) {
+				return res.status(401).json({ message: "unauthorized" });
+			}
+			const token = createToken();
+			let now = Date.now();
+			let session = new sessionModel({
+				user: req.body.username,
+				ttl: now + time_to_live_diff,
+				token: token
+			});
+			session.save(function (err) {
+				if (err) {
+					console.log("failed to save session in login, err: " + err);
+					return res.status(500).json({ message: "internal server error" });
+				}
+				return res.status(200).json({ token: token });
+			});
+		});
+	});
 });
 
 app.post("/logout",function(req,res) {
 	if(!req.headers.token){
-		return res.status(404).json({message:"not found"})
+		return res.status(404).json({message:"Not found"})
 	}
-	for(let i =0;i<loggedSessions.length;i++){
-		if(req.headers.token===loggedSessions[i].token){
-			loggedSessions.splice(i,1);
-			return res.status(200).json({message:"succes"})
+	
+	sessionModel.deleteOne({ token: req.headers.token }, function (err, _results) {
+		if (err) {
+			console.log("failed to remove session, err: " + err);
 		}
-	}
-	return res.status(404).json({message:"not found"})
+		return res.status(200).json({ message: "success" });
+	});
 })
 
 
