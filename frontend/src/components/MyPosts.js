cbc8cd998e0e5b2ebc65731e5643f2de9415e93f
Petri Kärki <pjkarabc@saunalahti.fi>
2022-07-21 09:27:13 +0300

    Added  navbar to MyPosts page

--- frontend/src/components/MyPosts.js
+++ frontend/src/components/MyPosts.js
@@ -1,5 +1,10 @@
+import NavigationBar from "./NavigationBar";
 const MyPosts = () => {
-
+    return(
+        <div style={{backgroundColor:"grey"}}>
+            <NavigationBar />
+        </div>
+    )
 }
 
 export default MyPosts;
